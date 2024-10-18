34732d3612de88e5bb527122b41cfb93556ea146
Sathish <sbabu@progress.com>
2024-10-18 13:57:26 +0530

    add integration tests
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/inspec-profile-with-path-dependency/controls/elasticsearch_integ_control.rb
+++ test/integration/inspec-profile-with-path-dependency/controls/elasticsearch_integ_control.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
+describe elasticsearch(ssl_verify: false) do
+  its("node_count") { should >= 3 }
+end
+
+describe elasticsearch do
+  its("node_name") { should include "node1" }
+  its("os") { should_not include "MacOS" }
+  its("version") { should cmp > "1.2.0" }
+end
 
 
